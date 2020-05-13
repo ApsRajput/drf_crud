98992aad8aba1a6b76ce4f4579df5a1980dce1b7
apsrajput <apsrajput008@gmail.com>
2020-05-13 19:39:14 +0530

    added generic views for friend

--- crud/views.py
+++ crud/views.py
@@ -4,6 +4,7 @@ from . import models
 from . import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 
 @api_view(["GET", "POST"])
 def hello_world(request):
@@ -30,4 +31,15 @@ class BelongingViewset(viewsets.ModelViewSet):
 class BorrowedViewset(viewsets.ModelViewSet):
     queryset = models.Borrowed.objects.all()
     serializer_class = serializers.BorrowedSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Generics
+class FriendGenericslc(generics.ListCreateAPIView):
+    queryset = models.Friend.objects.all()
+    serializer_class = serializers.FriendSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class FriendGenericsrud(generics.ListCreateAPIView):
+    queryset = models.Friend.objects.all()
+    serializer_class = serializers.FriendSerializer
     permission_classes = [permissions.IsAuthenticated]
